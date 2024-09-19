c22086dac431c3fc5689ec29711db3588ba76951
marcsilva <marciliodevjava@gmail.com>
2024-09-18 21:57:20 -0300

    Identação Cliente

--- src/br/com/alura/Cliente.java
+++ src/br/com/alura/Cliente.java
@@ -4,13 +4,13 @@ public class Cliente {
     private String nome;
     private Long cpf;
 
-    public Cliente(String nome, String cpf){
+    public Cliente(String nome, String cpf) {
         this.nome = nome;
         this.cpf = this.montarCpf(cpf);
     }
 
     private Long montarCpf(String cpf) {
-        String formatter = cpf.replace(".","").replace("-","").replace(" ","");
+        String formatter = cpf.replace(".", "").replace("-", "").replace(" ", "");
         return Long.valueOf(formatter);
     }
 
