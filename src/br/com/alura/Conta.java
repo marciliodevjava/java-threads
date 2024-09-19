010f683655001736daadbf0f61a9ef0ee985910d
marcsilva <marciliodevjava@gmail.com>
2024-09-18 21:57:25 -0300

    Identação Conta

--- src/br/com/alura/Conta.java
+++ src/br/com/alura/Conta.java
@@ -3,12 +3,13 @@ package br.com.alura;
 import java.math.BigDecimal;
 import java.util.Random;
 
-public class Conta{
+public class Conta {
     private static Random random = new Random();
     private Cliente cliente;
     private BigDecimal saldo;
     private Long agencia;
     private Long numero = random.nextLong(100);
+
     public Conta(Cliente cliente, BigDecimal saldoInicial) {
         this.saldo = saldoInicial;
         this.cliente = cliente;
